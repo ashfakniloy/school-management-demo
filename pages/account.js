6fb21026b6b374339acb0d78b812c767ce20f2ec
niloy <niloy@gmail.com>
2022-07-28 17:25:02 +0600

    added details

--- pages/account.js
+++ pages/account.js
@@ -9,31 +9,35 @@ const users = [
     id: 1,
     name: "Steven Johnson",
     role: "Super Admin",
+    gender: "Male",
     image: "/images/user2.jpg",
   },
   {
     id: 2,
     name: "John Doe",
     role: "Admin",
+    gender: "Male",
     image: "/images/user2.jpg",
   },
   {
     id: 3,
     name: "Random User",
     role: "User",
+    gender: "Female",
     image: "/images/user2.jpg",
   },
   {
     id: 4,
     name: "Random User 2",
     role: "User",
+    gender: "Female",
     image: "/images/user2.jpg",
   },
 ];
 
 function AddEmail() {
   // const [showDetails, setShowDetails] = useState("");
-  const [userDetails, setUserDetails] = useState([]);
+  const [userDetails, setUserDetails] = useState(users[0]);
 
   console.log(userDetails);
 
@@ -57,7 +61,9 @@ function AddEmail() {
                   <div
                     key={user.id}
                     onClick={() => setUserDetails(user)}
-                    className=" odd:bg-teal-100 even:bg-cyan-100 hover:bg-slate-200 transition duration-300 rounded cursor-pointer"
+                    className={`odd:bg-teal-100 even:bg-cyan-100 hover:bg-slate-200 transition duration-300 rounded cursor-pointer ${
+                      user.id === userDetails.id ? "bg-slate-200" : ""
+                    }`}
                   >
                     <div className="">
                       <div className="px-7 py-5 flex items-center gap-6 ">
@@ -81,20 +87,35 @@ function AddEmail() {
             </div>
           </div>
 
-          <div className="bg-white py-10 shadow-md  w-[800px]">
+          <div className="flex-1 bg-white py-10 shadow-md">
             <h1 className="px-10 text-xl font-semibold text-slate-800">
               User Details
             </h1>
-            <div className="m-10 flex">
-              <Image
-                src={userDetails.image}
-                alt="user"
-                width={250}
-                height={250}
-                className="rounded-full"
-              />
+            <div className="m-10  flex gap-10">
+              <div className="">
+                <Image
+                  src={userDetails.image}
+                  alt="user"
+                  width={250}
+                  height={250}
+                  className="rounded-full"
+                />
+              </div>
+              <div className="space-y-8">
+                <div className="grid grid-cols-2 gap-20">
+                  <p className="text-slate-500">Name:</p>
+                  <p className="">{userDetails.name}</p>
+                </div>
+                <div className="grid grid-cols-2 gap-20">
+                  <p className="text-slate-500">User Type:</p>
+                  <p className="">{userDetails.role}</p>
+                </div>
+                <div className="grid grid-cols-2 gap-20">
+                  <p className="text-slate-500">Gender:</p>
+                  <p className="">{userDetails.gender}</p>
+                </div>
+              </div>
             </div>
-            <div className="">{userDetails.name}</div>
           </div>
         </div>
       </div>
